9d465ef2d8ffdf67f4ae49047b1e552a4a4770ef
Ayat <eng.hard@yahoo.com>
2024-03-26 20:59:53 +0000

    Task1 created

---
+++ src/ayat/week1/task1_OddOrEven_ayat.java
@@ -0,0 +1,18 @@
+package ayat.week1;
+
+public class task1_OddOrEven_ayat {
+    public static void main(String[] args) {
+
+        int num = 10;
+
+        if (num % 2 == 0) {
+            System.out.println(num + " is even");
+
+        }
+        else {
+            System.out.println(num + " is odd");
+        }
+
+
+    }
+}
