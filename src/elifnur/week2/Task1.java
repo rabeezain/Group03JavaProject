702bc55c2d6209a5683683f75d65b6e13e575a74
elifnuratasayar <atasayarelifnur@gmail.com>
2024-04-04 12:18:25 +0100

    week2 taskss

--- src/elifnur/week2/Task1.java
+++ src/elifnur/week2/Task1.java
@@ -5,14 +5,13 @@ import java.io.FilterOutputStream;
 public class Task1 {
     public static void main(String[] args) {
 
-        int x=5;
-        int y=4;
-       x=x+y;
-       y=x-y;
-        x=x-y ;
+        int x = 5;
+        int y = 4;
+        x = x + y;
+        y = x - y;
+        x = x - y;
         System.out.println(x);
         System.out.println(y);
-
 
 
     }
--- src/elifnur/week2/task3.java
+++ src/elifnur/week2/task3.java
@@ -2,30 +2,27 @@ package elifnur.week2;
 
 public class task3 {
     public static void main(String[] args) {
-        int N=24;
-        for (int i = 1; i <N; i++) {
-            if ((i % 2 == 0 && i % 3 == 0) ) {
+        int N = 24;
+        for (int i = 1; i < N; i++) {
+            if ((i % 2 == 0 && i % 3 == 0)) {
                 System.out.println("CodilityTest");
 
             } else if (i % 3 == 0) {
                 System.out.println("Test");
 
 
-            }else if  (i % 3 == 0 && i % 5 == 0){
+            } else if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("TestCoders");
 
             } else if (i % 2 == 0) {
 
                 System.out.println("Codility");
-            }
-            else if (i % 5 == 0) {
-                System.out.println("Coders");}
+            } else if (i % 5 == 0) {
+                System.out.println("Coders");
+            } else if (i % 5 == 0 && i % 2 == 0) {
+                System.out.println("Codi1tyCoders");
 
-            else if (i % 5 == 0 && i%2==0) {
-                    System.out.println("Codi1tyCoders");
-
-                }
-            else {
+            } else {
                 System.out.println(" " + i + " ");
             }
         }
