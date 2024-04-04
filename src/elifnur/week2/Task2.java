9a8be3665e8753c86cffad6bea1140834a8d2a7c
elifnuratasayar <atasayarelifnur@gmail.com>
2024-04-04 12:18:25 +0100

    week2 taskss

--- src/elifnur/week2/Task2.java
+++ src/elifnur/week2/Task2.java
@@ -7,24 +7,24 @@ public class Task2 {
         String divisibleBy5 = "";
         String divisibleBy15 = "";
         for (int i = 1; i < 101; i++) {
-            if (i%3==0 && i%5==0 && i%15==0){
+            if (i % 3 == 0 && i % 5 == 0 && i % 15 == 0) {
                 divisibleBy15 += i + " ";
 
-            } else if (i%3==0 ){
+            } else if (i % 3 == 0) {
                 divisibleBy3 += i + " ";
 
 
-            }else if (i%5==0 ){
+            } else if (i % 5 == 0) {
                 divisibleBy5 += i + " ";
 
             }
-            }
+        }
         System.out.println("divisibleBy3:" + divisibleBy3);
         System.out.println("divisibleBy5: " + divisibleBy5);
         System.out.println("divisibleBy15: " + divisibleBy15);
-        }
+    }
 
 
-        }
+}
 
 
