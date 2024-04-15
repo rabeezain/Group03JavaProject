40c01ad8d03b9ba1683f4aaa4054180b85f0ac35
Tatjana <tnonkovic@gmail.com>
2024-04-15 23:30:36 +0200

    add changes

---
+++ src/tatjana/week01/FINRA.java
@@ -0,0 +1,23 @@
+package tatjana.week01;
+
+import java.util.Scanner;
+
+public class FINRA {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your dividend number: ");
+        int dividend = input.nextInt();
+
+        System.out.println("Enter your divisor number: ");
+        int divisor = input.nextInt();
+
+        int quotioent = 0;
+        while(dividend>=divisor){
+            dividend -= divisor;
+            quotioent++;
+        }
+        System.out.println("Quoetient is " + quotioent + " with remainder " + dividend);
+    }
+    }
+
